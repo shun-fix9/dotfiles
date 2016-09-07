22d5d07984f1e5af24e9e4c6956e1c23931faacb
shun <shun.fix9@gmail.com>
2016-09-07 22:03:27 +0000

    build でレジストリへの push を追加

--- dockerfiles/devel/build.sh
+++ dockerfiles/devel/build.sh
@@ -9,5 +9,13 @@ dockerfile_build_pre(){
     sed -i "s/^# VERSION for docker :.*/# VERSION for docker : $version/" bin/development-environment-setup
   fi
 }
+dockerfile_build_post(){
+  if [ -f $HOME/.docker_buildrc ]; then
+    . $HOME/.docker_buildrc
+    if [ -n "$DOCKER_BUILD_REGISTRY" ]; then
+      dockerfile_build_push $DOCKER_BUILD_REGISTRY
+    fi
+  fi
+}
 
 dockerfile_build_image shun/devel "$@"
