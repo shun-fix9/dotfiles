4518892d26e35d92eb57eb73c92e892658192c91
shun <shun.fix9@gmail.com>
2016-10-04 13:50:21 +0000

    fix dockerfile

--- dockerfiles/devel/build.sh
+++ dockerfiles/devel/build.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
 
 . dockerfile-build-functions.sh
-
-mode=$1
-
-dockerfile_build_pre(){
-  sed -i "s/^# VERSION for docker :.*/# VERSION for docker : $version/" bin/development-environment-setup
-}
-dockerfile_build_post(){
-  if [ -f $HOME/.docker_build/rc ]; then
-    . $HOME/.docker_build/rc
-    if [ -n "$DOCKER_BUILD_REGISTRY" ]; then
-      dockerfile_build_push $DOCKER_BUILD_REGISTRY
-    fi
-  fi
-}
-
 dockerfile_build_image shun/devel "$@"
